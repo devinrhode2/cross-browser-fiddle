f442ed3844045f2dc53c67d4361ef7993e508bec
Devin Rhode <devinrhode2@gmail.com>
2013-01-31 10:37:12 -0800

    boom

---
+++ test/cross-browser-fiddle.js
@@ -0,0 +1,30 @@
+var test = require('tape');
+
+test('fiddlin', function (t) {
+  var methods = [
+      'addRange'
+    , 'collapse'
+    , 'collapseToEnd'
+    , 'collapseToStart'
+    , 'constructor'
+    , 'containsNode'
+    , 'deleteFromDocument'
+    , 'empty'
+    , 'extend'
+    , 'getRangeAt'
+    , 'modify'
+    , 'removeAllRanges'
+    , 'selectAllChildren'
+    , 'setBaseAndExtent'
+    , 'setPosition'
+  ];
+  console.log(t);
+  methods.forEach(function forEachMethod(method) {
+    if (window.Selection.prototype[method]) {
+      console.log('has ' + method);
+    } else {
+      console.log('doesnt have: '  + method);
+    }
+  });
+  t.end();
+});
